762c805814d25d539498bfe1a9b0b86aa944aec3
Marcillius <marciliordsantos@gmail.com>
2025-03-28 14:11:57 -0300

    primeiro commit

---
+++ src/main/java/com/novoprojeto/novoprojeto/NovoprojetoApplication.java
@@ -0,0 +1,13 @@
+package com.novoprojeto.novoprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoprojetoApplication.class, args);
+	}
+
+}
